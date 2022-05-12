04efbf4d883a4be41eff4b26f7177bf5c02107cd
David Brockwell <dbrockwell@my.wctc.edu>
2022-05-12 13:57:34 -0500

    Added current room interactions

--- src/edu/wctc/Maze.java
+++ src/edu/wctc/Maze.java
@@ -5,4 +5,35 @@ public class Maze {
     private Player player;
     private boolean isFinished;
 
+    public Maze(Player player) {
+        this.player = player;
+    }
+
+    public String ExitCurrentRoom() {
+        if (currentRoom.getExit() != null) {
+            isFinished = true;
+            return currentRoom.getExit();
+        }
+        else {
+            return "You can not exit the cave here";
+        }
+    }
+
+    public String InteractCurrentRoom() {
+        if (currentRoom.getInteract() != null) {
+            return currentRoom.getInteract();
+        }
+        else {
+            return "You can not interact with the current room";
+        }
+    }
+
+    public String LootCurrentRoom() {
+        if (currentRoom.getLoot() != null) {
+            return currentRoom.getLoot();
+        }
+        else {
+            return "You can not loot the current room";
+        }
+    }
 }
